bd7f719ea3da0c8977b6a03ba59e5a8ee80ffeb9
abhay385 <57624703+abhay385@users.noreply.github.com>
2024-01-30 17:43:54 +0000

    datatype conversion

---
+++ 01_basics/03_conversionAndOperators.js
@@ -0,0 +1,109 @@
+let score = 33
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let score1 = "33"
+
+//console.log(typeof score1)
+
+
+//let valueInNumber= Number(score1)   // conversion str to no.
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+let str = "33abc"
+
+// console.log(typeof str)
+// let valueInNumber= Number(str)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)              // Nan  -> Not a Number
+
+
+// let marks = null
+// console.log(typeof marks)
+// let valueInNumber= Number(marks)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) 
+
+// let marks1 = undefined
+// console.log(typeof marks1)
+// let valueInNumber= Number(marks1)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) 
+
+
+// let marks2 = true
+// console.log(typeof marks2)
+// let valueInNumber= Number(marks2)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) 
+
+
+// let marks3 = "welcome"
+// console.log(typeof marks3)
+// let valueInNumber= Number(marks3)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) 
+
+/* Notes for Number Conversion(any to number)
+
+input      |      output
+null       |        0 (zero)
+undefined  |        NaN  (not a number)
+"welcome"  |        NaN
+true       |        1
+"33"       |        33
+
+
+*/
+
+
+// conversion to string 
+let someNum = 33
+
+// let stringNumber = String(someNum)
+// console.log(typeof someNum)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let isLoggedIn = " "
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let isLoggedIn = "abhay"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+/* Notes for conversion  any to boolean
+
+input       |      outputs
+1           |       true
+0           |       false
+""          |       false
+"  "        |       true
+"abhay"     |       true
+
+*/
