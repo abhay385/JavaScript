586805182017cb5a33cf0aa6eddb0d8f229ea98b
abhay385 <57624703+abhay385@users.noreply.github.com>
2024-02-02 11:38:12 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,17 @@
+let name = "Javascript"
+let year = 2024
+//console.log(typeof name)
+//console.log(name)
+//console.log(`${name} is a very popular language in year ${year} !`)
+let par = new String("Hello and Welcome")
+//console.log(par)
+console.log(par.length)
+console.log(par.toUpperCase())
+console.table(par.charAt(3))
+console.log(par.indexOf('W'))
+console.log(par.__proto__)
+console.table(par.substring(0,6))
+console.log(par.slice(0,9))
+console.log(par.replace("and", 'g'))
+console.log(par.includes('abc'))        // check if abc is there in string
+console.log(par.split(' '))             // convert string into array w.r.t sapce here
