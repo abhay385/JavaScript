3376485111a338494897e46241418641f059d7f6
abhay385 <57624703+abhay385@users.noreply.github.com>
2024-02-03 17:15:18 +0000

    maths

---
+++ 01_basics/06_numbers_Maths.js
@@ -0,0 +1,46 @@
+// let num = 399               // bydefault it is number
+// console.log(typeof num)
+
+// let balance = new Number(1234)
+// console.log(balance)
+// console.log(balance.toFixed(2))
+// console.log(balance.toPrecision(3))
+// console.log(balance.toString().length)
+
+// const salary = 100000000
+// console.log(salary.toLocaleString())
+// console.log(salary.toLocaleString('en-In'))
+
+
+// console.group(Number.MAX_VALUE)
+// console.group(Number.MIN_VALUE)
+// console.group(Number.MAX_SAFE_INTEGER)
+
+// EXPLORE CONSOLE FOR MORE PROPERTIES
+
+
+// +++++++++++++++++++++++++++++ MATHS +++++++++++
+console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.ceil(3.01))
+// console.log(Math.floor(3.99))
+// console.log(Math.round(5.51))
+// console.log(Math.round(5.49))
+// console.log(Math.fround(8.9))
+
+// console.log(Math.max(2,4,6,8))
+// console.log(Math.min(2,4,6,8))
+// console.log(Math.random())
+// console.log(Math.random()*10)
+console.log(Math.random()*10 + 1)
+console.log(Math.floor(Math.random()*10 + 1))  // check in details
+
+
+max = 20
+min = 10
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
+
+
+
+
